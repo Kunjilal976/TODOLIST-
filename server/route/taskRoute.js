011dbf67ebeba659bbf03e18e9f090e8538d148e
Kunji Lal Meena <kunjilal976@gmail.com>
2024-05-25 22:48:27 +0530

    n

--- server/route/taskRoute.js
+++ server/route/taskRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const taskController = require('../controller/taskController');
 
 // Define routes
-router.get('/', taskController.getAllTasks);
-router.post('/', taskController.createTask);
+router.get('/tasks', taskController.getAllTasks);
+router.post('/tasks', taskController.createTask);
 router.get('/:id', taskController.getTaskById);
 router.delete('/:id', taskController.deleteTaskById);
 router.put('/:id', taskController.updateTaskById);
